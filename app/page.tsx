6dea0178ac994b4e521cac2bb32f647d658b7d39
MauryaRitesh <ritesh.k.m786@gmail.com>
2025-01-23 13:50:25 +0530

    add lhci

--- app/page.tsx
+++ app/page.tsx
@@ -89,6 +89,7 @@ export default function Home() {
           <Image
             aria-hidden
             src="/globe.svg"
+            alt="Globe icon"
             width={16}
             height={16}
           />
