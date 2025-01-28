4577faff9b4d307c3e4de6421f1bd9c5c7e3c87c
MauryaRitesh <ritesh.k.m786@gmail.com>
2025-01-28 11:30:48 +0530

    add new pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <h1>This is blog page!</h1>
+    return <h1>This is blog page! Hello.</h1>
   }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <h1>This is Products page!</h1>
+    return <h1>This is Products page! Hello.</h1>
   }
